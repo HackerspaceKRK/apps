13af445a272522f45f3cc9c5183c98e5a3ba7777
Hubert Bryłkowski <hubert@brylkowski.com>
2023-08-22 14:37:18 +0200

    switch saving rendered images to internal minio

--- building-automation/save-images-script.sh
+++ building-automation/save-images-script.sh
@@ -5,7 +5,7 @@ IMAGES_DIR=/tmp/images
 ZONEMINDER_LOGIN=$(cat /run/secrets/zoneminder_login)
 ZONEMINDER_PASSWORD=$(cat /run/secrets/zoneminder_password)
 
-mkdir -p ${IMAGES_DIR}
+mkdir -p ${IMAGES_DIR}/charts
 
 declare -A rooms
 
@@ -24,8 +24,8 @@ function download(){
       echo "ID: $CAMERA_ID, Room: ${rooms[$CAMERA_ID]}"
   done
 
-  curl 'https://grafana.apps.hskrk.pl/render/d-solo/efd454c7-2714-4507-b5b5-188e1feceeb7/public-network-stats?orgId=1&panelId=2&width=1000&height=500&tz=Europe%2FWarsaw' > ${IMAGES_DIR}/public-network-stats.png
-  curl 'https://grafana.apps.hskrk.pl/render/d-solo/cc375ef7-341d-4ded-9a3d-73b886f46c08/power-consumption?orgId=1&panelId=1&width=1000&height=500&tz=Europe%2FWarsaw' > ${IMAGES_DIR}/power-consumption.png
+  curl 'https://grafana.apps.hskrk.pl/render/d-solo/efd454c7-2714-4507-b5b5-188e1feceeb7/public-network-stats?orgId=1&panelId=2&width=1000&height=500&tz=Europe%2FWarsaw' > ${IMAGES_DIR}/charts/public-network-stats.png
+  curl 'https://grafana.apps.hskrk.pl/render/d-solo/cc375ef7-341d-4ded-9a3d-73b886f46c08/power-consumption?orgId=1&panelId=1&width=1000&height=500&tz=Europe%2FWarsaw' > ${IMAGES_DIR}/charts/power-consumption.png
 
   aws s3 --endpoint-url=${S3_ENDPOINT} sync ${IMAGES_DIR}/ s3://${IMAGES_BUCKET}/
 }
